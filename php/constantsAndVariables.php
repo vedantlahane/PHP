61a4d2b05863a231bdf82e194a82f846b46341a9
Vedant Lahane <vedantanillahane@gmail.com>
2024-09-11 23:37:24 +0530

    dynamic variable assignment

--- php/constantsAndVariables.php
+++ php/constantsAndVariables.php
@@ -14,3 +14,14 @@ echo FIRST_NAME; // Output: Joe
 const LAST_NAME = 'Smith';
 
 echo LAST_NAME; // Output: Smith
+
+if(true){
+    define('MIDDLE_NAME', 'John');
+    echo MIDDLE_NAME; // Output: John
+    const MIDDLE_NAME = 'Doe'; // Output: Fatal error: Cannot redeclare MIDDLE_NAME
+    const lastname = 'Doe'; // Output: Fatal error: Constant name must be a valid identifier
+}
+
+$foo = 'bar';
+$$foo = 'baz';
+echo $bar; // Output: baz
